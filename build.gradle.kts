d469826b594ab45e7547499e1e2e33cb9a4a0d2e
Mamadou-Dardaye-Bah <2393818@cegepmontpetit.ca>
2026-09-08 12:52:42 -0400

    Exercice ValidationCourriel 50% Complet

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,13 @@ dependencies {
     testImplementation(kotlin("test"))
     // Source: https://mvnrepository.com/artifact/org.jsoup/jsoup
     implementation("org.jsoup:jsoup:1.23.1")
+    // Source: https://mvnrepository.com/artifact/org.apache.commons/commons-email
+    implementation("org.apache.commons:commons-email:1.5")
+    // Source: https://mvnrepository.com/artifact/email-validator/email-validator
+    implementation("email-validator:email-validator:0.1")
+    // Source: https://mvnrepository.com/artifact/commons-validator/commons-validator
+    implementation("commons-validator:commons-validator:1.11.0")
+
 }
 
 kotlin {
---
+++ src/main/kotlin/ValidationCourriel.kt
@@ -0,0 +1,8 @@
+package org.Bah
+
+fun main(args : List<String>) {
+    org.apache.commons.validator.Validator
+    for(mail : String in args) {
+
+    }
+}
